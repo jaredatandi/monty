0889f26f566b3642d14aade2fb8588ecf7fadafd
jaredatandi <jaredatandi07@gmail.com>
2022-10-06 07:43:28 +0300

    Add a parser function

---
+++ tokens.c
@@ -0,0 +1,63 @@
+#include "structs.h"
+#include "errors.h"
+#include "lists.h"
+
+int count_ws(char *str)
+{
+	int count, w, flag;
+
+	flag = 0;
+	w = 0;
+
+	for (count = 0; str[count] != '\0'; count++)
+	{
+		if (str[count] == ' ')
+			flag = 0;
+		else if (flag == 0)
+		{
+			flag = 1;
+			w++;
+		}
+	}
+	return (w);
+}
+
+char **tokens(char *str)
+{
+	char **av, *tmp;
+	int i, k = 0, len = 0, c = 0, w, start, end;
+
+	len = strlen(str);
+	w = count_ws(str);
+	if (w == 0)
+		return (NULL);
+
+	av = (char **) malloc(sizeof(char *) * (w + 1));
+	if (av == NULL)
+		return (NULL);
+
+	for (i = 0; i <= len; i++)
+	{
+		if (isspace(str[i]) || str[i] == '\0' || str[i] == '\n')
+		{
+			if (c)
+			{
+				end = i;
+				tmp = (char *) malloc(sizeof(char) * (c + 1));
+				if (tmp == NULL)
+					return (NULL);
+				while (start < end)
+					*tmp++ = str[start++];
+				*tmp = '\0';
+				av[k] = tmp - c;
+				k++;
+				c = 0;
+			}
+		}
+		else if (c++ == 0)
+			start = i;
+	}
+	av[k] = NULL;
+
+	return (av);
+}
