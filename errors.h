4e095d8d1dbe1266e76583e170f41feb0bbb6d36
jaredatandi <jaredatandi07@gmail.com>
2022-10-06 23:25:45 +0300

    Update errors

--- errors.h
+++ errors.h
@@ -3,10 +3,21 @@
 
 
 #define USAGE "USAGE: monty file\n"
-#define FILE_ERROR "Error: can't open file %s\n"
-#define PUSH_FAIL "L%u: usage: push integer\n"
+#define FILE_ERROR "Error: Can't open file %s\n"
 #define UNKNOWN "L%u: unknown instruction %s\n"
 #define MALLOC_FAIL "Error: malloc failed\n"
+#define PUSH_FAIL "L%u: usage: push integer\n"
+#define PINT_FAIL "L%u: can't pint, stack empty\n"
+#define POP_FAIL "L%u: can't pop an empty stack\n"
+#define SWAP_FAIL "L%u: can't swap, stack too short\n"
+#define ADD_FAIL "L%u: can't add, stack too short\n"
+#define SUB_FAIL "L%u: can't sub, stack too short\n"
+#define DIV_FAIL "L%u: can't div, stack too short\n"
+#define DIV_ZERO "L%u: division by zero\n"
+#define MUL_FAIL "L%u: can't mul, stack too short\n"
+#define MOD_FAIL "L%u: can't mod, stack too short\n"
+#define PCHAR_FAIL "L%u: can't pchar, stack empty\n"
+#define PCHAR_RANGE "L%u: can't pchar, value out of range\n"
 
 #define UNUSED(x) void(x)
 
