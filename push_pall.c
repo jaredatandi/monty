7103edca0ba6ba8815dbf8140d7f35096cae7771
jaredatandi <jaredatandi07@gmail.com>
2022-10-06 20:14:57 +0300

    Change formal arguments to take a stack struct

--- push_pall.c
+++ push_pall.c
@@ -7,19 +7,22 @@
  * @line_number: line number on the file
  */
 
-void push_h(stack_t **stack __attribute__((unused)), unsigned int line_number)
+void push_h(stack_t **stack, unsigned int line_number)
 {
-	int i;
+	int i, n = 0;
+	stack_t *new;
 
-
+	/** Check if any data is provided **/
 	if (infor.words[1] == NULL)
 	{
 		fprintf(stderr, PUSH_FAIL, line_number);
 		release_all(1);
 		exit(EXIT_FAILURE);
 	}
-	for (i = 0; i < infor.words[1][i]; ++i)
+	for (i = 0; i < infor.words[1][i]; i++)
 	{
+		/** Check for other ASCII chars other than
+		 * Words and numbers **/
 		if (isalpha(infor.words[1][i]) != 0)
 		{
 			fprintf(stderr, PUSH_FAIL, line_number);
@@ -28,9 +31,16 @@ void push_h(stack_t **stack __attribute__((unused)), unsigned int line_number)
 		}
 	}
 
+	n = atoi(infor.words[1]);
 	if (infor.flag == 0)
-		printf("add to stack\n");
+		new = add_top(stack, n); 
 	else if (infor.flag == 1)
-		printf("add to end");
+		new = add_end(stack, n);
+	if (!new)
+	{
+		fprintf(stderr, MALLOC_FAIL);
+		release_all(1);
+		exit(EXIT_FAILURE);
+	}
 }
 
