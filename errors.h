20d0432cce4f0f1a1c52b5924b422a3655be08f6
jaredatandi <jaredatandi07@gmail.com>
2022-10-06 07:39:29 +0300

    Add more errors definition

--- errors.h
+++ errors.h
@@ -3,5 +3,8 @@
 
 #define USAGE "USAGE: monty file\n"
 #define FILE_ERROR "ERROR: can't open file %s\n"
+#define PUSH_FAIL "L%u: usage: push integer\n"
+#define UNKNOWN "L%u: unknown instruction %s\n"
+#define MALLOC_FAIL "Error: malloc failed\n"
 
 #endif /* ERRORS_H */
