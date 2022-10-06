c88d6e9211c6cdaaa61efa7a5f1d7f9bfe662552
jaredatandi <jaredatandi07@gmail.com>
2022-10-07 01:48:23 +0300

    Improve header file readability

--- monty.h
+++ monty.h
@@ -9,8 +9,9 @@
 #include <unistd.h>
 #include <ctype.h>
 
-
+/*===================================================================*/
 /** Structs **/
+/*===================================================================*/
 
 /**
  * struct stack_s - doubly linked list represenation of a stack (or queue)
@@ -78,7 +79,9 @@ extern infor_s infor;
 
 #define INFOR_INIT {NULL, NULL, NULL, NULL, 0}
 
+/*===================================================================*/
 /** Handlers **/
+/*===================================================================*/
 void push_h(stack_t **stack, unsigned int line_number);
 void pall_h(stack_t **stack, unsigned int line_number);
 void pint_h(stack_t **stack, unsigned int line_number);
@@ -86,18 +89,24 @@ void pop_h(stack_t **stack, unsigned int line_number);
 void swap_h(stack_t **stack, unsigned int line_number);
 void add_h(stack_t **stack, unsigned int line_number);
 
+/*===================================================================*/
 /** Helpers **/
+/*===================================================================*/
 int count_ws(char *str);
 char **tokens(char *str);
 void run_cmd(args_a *args);
 void (*get_func(char **parse))(stack_t **, unsigned int);
 
+/*===================================================================*/
 /** Cleaners **/
+/*===================================================================*/
 void release(char **args);
 void release_all(int all);
 void free_stack(char *stack);
 
+/*===================================================================*/
 /** operations **/
+/*===================================================================*/
 stack_t *add_top(stack_t **h, const int n);
 stack_t *add_end(stack_t **h, const int n);
 int delete_node(stack_t **head, unsigned int index);
