8ad20a874f7ed423af021e10431b70c66bab3ff3
jaredatandi <jaredatandi07@gmail.com>
2022-10-06 23:52:09 +0300

    Add pint handling

--- ops.c
+++ ops.c
@@ -85,3 +85,41 @@ size_t p_list(const stack_t *head)
 
 	return (nodes);
 }
+
+/**
+ * delet_node - deletes a node at a specific index
+ * @head: pointer to the list
+ * @index: index of the node to delete
+ *
+ * Return: 1 on success, -1 on error
+ */
+int delete_node(stack_t **head, unsigned int index)
+{
+	stack_t *temp = *head;
+	unsigned int i = 0;
+
+	if (!index)
+	{
+		(*head) = temp->next;
+		if (temp->next)
+			temp->next->prev = NULL;
+		temp->next = NULL;
+		free(temp);
+		return (1);
+	}
+
+	while (i < index)
+	{
+		temp = temp->next;
+		i++;
+		if (!temp)
+			return (0);
+	}
+	
+	temp->prev->next = temp->next;
+	if (temp->next)
+		temp->next->prev = temp->prev;
+	free(temp);
+
+	return (1);
+}
