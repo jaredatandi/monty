b9494e97469271772357f349958157a6216d7fd2
jaredatandi <jaredatandi07@gmail.com>
2022-10-06 20:11:44 +0300

    Comment labels for different files

--- monty.h
+++ monty.h
@@ -78,14 +78,23 @@ extern infor_s infor;
 
 #define INFOR_INIT {NULL, NULL, NULL, NULL, 0}
 
+/** Handlers **/
 void push_h(stack_t **stack, unsigned int line_number);
+
+/** Helpers **/
 int count_ws(char *str);
 char **tokens(char *str);
 void run_cmd(args_a *args);
 void (*get_func(char **parse))(stack_t **, unsigned int);
+
+/** Cleaners **/
 void release(char **args);
 void release_all(int all);
 void free_stack(char *stack);
 
+/** operations **/
+
+stack_t *add_top(stack_t **h, const int n);
+stack_t *add_end(stack_t **h, const int n);
 
 #endif /* end of include guard: STRUCTS_H*/
