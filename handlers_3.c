940f165de6da76c446268841e31201cbc98f248e
jaredatandi <jaredatandi07@gmail.com>
2022-10-09 11:23:38 +0300

    Add handlers_3.c file

---
+++ handlers_3.c
@@ -0,0 +1,38 @@
+#include "monty.h"
+#include "errors.h"
+
+
+/**
+ * mul_h - multiplication handling
+ * @stack: pointer to the stack
+ * @line_number: line number on the file
+ * Description: mul function multiplies the second top element
+ * of the stack with the top element of the stack
+ */
+void mul_h(stack_t **stack, unsigned int line_number)
+{
+	int res;
+	stack_t *node = NULL;
+	stack_t *node_0, *node_1;
+
+	if (list_len(*stack) < 2)
+	{
+		dprintf(STDERR_FILENO, MUL_FAIL, line_number);
+		release_all(1);
+		exit(EXIT_FAILURE);
+	}
+	node_0 = get_node(*stack, 0);
+	node_1 = get_node(*stack, 1);
+
+	res = node_0->n * node_1->n;
+	delete_node(stack, 0);
+	delete_node(stack, 0);
+	node = add_top(stack, res);
+
+	if (!node)
+	{
+		dprintf(STDERR_FILENO, MALLOC_FAIL);
+		release_all(1);
+		exit(EXIT_FAILURE);
+	}
+}
